4215c6c336841bbfa927d7e09eb51e43859d9393
Caleb Nasio <nascal3@gmail.com>
2024-01-09 14:18:54 +0300

    created the sign-up dynamic form

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
+  imports: [
+    ReactiveFormsModule,
+    RouterLink
+  ],
   standalone: true
 })
 export class RegisterComponent {
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required,],
+    email: ['', Validators.required,]
+  });
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    console.log('Form>>>', this.form.value);
+  }
 }
