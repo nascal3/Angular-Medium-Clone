0219d95eda76c5b5b29d4b700863088a9b910d5b
Caleb Nasio <nascal3@gmail.com>
2024-01-09 16:08:16 +0300

    Added action to the submit register auth function

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core'
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
 import { RouterLink } from '@angular/router'
+import { Store } from '@ngrx/store'
+
+import { register } from '../../store/actions'
+import { RegisterRequestInterface } from '../../types/registerRequest.interface'
 
 @Component({
   selector: 'mc-register',
@@ -14,9 +18,16 @@ export class RegisterComponent {
     password: ['', Validators.required],
     email: ['', Validators.required],
   })
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private store: Store
+  ) {}
 
   onSubmit() {
     console.log('Form>>>', this.form.value)
+    const request: RegisterRequestInterface = {
+      user: this.form.getRawValue(),
+    }
+    this.store.dispatch(register({ request }))
   }
 }
