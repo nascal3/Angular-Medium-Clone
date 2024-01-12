4f7c80d02d9e1ac6626fe391a65a571697c5cd4a
Caleb Nasio <nascal3@gmail.com>
2024-01-12 13:12:57 +0300

    Added redirect to effects.ts

---
+++ src/app/shared/services/persistance.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core'
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class PersistanceService {
+  set(key: string, data: unknown): void {
+    try {
+      localStorage.setItem(key, JSON.stringify(data))
+    } catch (e) {
+      console.error('Error while setting local storage', e);
+    }
+  }
+
+  get(key: string): unknown {
+    try {
+      const localStorageItem = localStorage.getItem(key)
+      return localStorageItem ? JSON.parse(localStorageItem) :null
+    } catch (e) {
+      console.error('Error while getting local storage', e)
+      return null
+    }
+  }
+  constructor() { }
+}
